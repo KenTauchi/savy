f50904fc2de39925c8838248b29c4d0eb08879a2
H J Vasoya <65635239+hjvasoya009@users.noreply.github.com>
2020-11-06 21:34:30 -0800

    Router Change & Map Change

--- front-end/src/components/contact/contact.js
+++ front-end/src/components/contact/contact.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Header from '../header/header';
-import Footer from '../footer/footer';
 
 class Contact extends React.Component {
 
@@ -28,38 +26,33 @@ class Contact extends React.Component {
     render() {
         return (
             <div>
-                <Header />
-                <div>
-                    <h2>If there is any question or feedback, feel free to reach out to us.</h2>
+                <h2>If there is any question or feedback, feel free to reach out to us.</h2>
 
-                    <div className="contact-section">
-                        <div className="contact-img">
-                            <img src="./images/contact.jpeg"></img>
-                        </div>
+                <div className="contact-section">
+                    <div className="contact-img">
+                        <img src="./images/contact.jpeg"></img>
+                    </div>
 
-                        <div className="contact-content">
+                    <div className="contact-content">
 
-                            <h3>Contact Us</h3>
+                        <h3>Contact Us</h3>
 
-                            <form id="contact-form" method="POST" onSubmit={this.contactSubmit}>
+                        <form id="contact-form" method="POST" onSubmit={this.contactSubmit}>
 
-                                <label htmlFor="name">Name</label>
-                                <input type="text" name="name" />
+                            <label htmlFor="name">Name</label>
+                            <input type="text" name="name" />
 
-                                <label htmlFor="email">Email</label>
-                                <input type="email" name="email" />
+                            <label htmlFor="email">Email</label>
+                            <input type="email" name="email" />
 
-                                <label htmlFor="message">Message</label>
-                                <textarea name="message" rows="3"></textarea>
+                            <label htmlFor="message">Message</label>
+                            <textarea name="message" rows="3"></textarea>
 
-                                <input type="submit" value="Submit" />
-                            </form>
+                            <input type="submit" value="Submit" />
+                        </form>
 
-                        </div>
                     </div>
                 </div>
-
-                <Footer />
             </div>
         );
     }
--- front-end/src/components/quiz/quiz.js
+++ front-end/src/components/quiz/quiz.js
@@ -3,31 +3,26 @@ import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { getQuizData } from "../../reducks/quiz/selectors";
 
-import Header from "../header/header";
-import Footer from "../footer/footer";
 import Question from "../quiz/question";
 import AnswerWindow from "./answer-window/AnswerWindow";
 
 import "./quiz.scss";
 
 const Quiz = () => {
-  const selector = useSelector((state) => state);
-  const quizData = getQuizData(selector);
+	const selector = useSelector((state) => state);
+	const quizData = getQuizData(selector);
 
-  return (
-    <div className="quiz-page">
-      <Header />
+	return (
+		<div className="quiz-page">
 
-      <div className="quiz-content">
-        {quizData.windowDis ? <div className="quiz-overlay"></div> : null}
-        <h1>Quiz</h1>
-        <Question />
-        {quizData.windowDis ? <AnswerWindow /> : null}
-      </div>
-
-      <Footer />
-    </div>
-  );
+			<div className="quiz-content">
+				{quizData.windowDis ? <div className="quiz-overlay"></div> : null}
+				<h1>Quiz</h1>
+				<Question />
+				{quizData.windowDis ? <AnswerWindow /> : null}
+			</div>
+		</div>
+	);
 };
 
 export default Quiz;
