67b9c39da2cdad601f9372ad90e0ec3b7cc317c1
H J Vasoya <65635239+hjvasoya009@users.noreply.github.com>
2020-11-10 21:45:27 -0800

    Testimonial CSS fixed

--- front-end/src/components/recycling-canada/testimonial/testimonial.js
+++ front-end/src/components/recycling-canada/testimonial/testimonial.js
@@ -11,66 +11,69 @@ import "../../../../node_modules/swiper/swiper.scss";
 SwiperCore.use([Pagination]);
 
 const Testimonial = () => {
-  const selector = useSelector((state) => state);
-  const testimonials = getTestimonials(selector);
-  const dispatch = useDispatch();
+	const selector = useSelector((state) => state);
+	const testimonials = getTestimonials(selector);
+	const dispatch = useDispatch();
 
-  const formatDate = (date) => {
-    const options = { year: "numeric", month: "long", day: "numeric" };
-    return new Date(date).toLocaleDateString(undefined, options);
-  };
+	const formatDate = (date) => {
+		const options = { year: "numeric", month: "long", day: "numeric" };
+		return new Date(date).toLocaleDateString(undefined, options);
+	};
 
-  useEffect(() => {
-    console.log("Testimonial API data fetch rendered");
+	useEffect(() => {
+		console.log("Testimonial API data fetch rendered");
 
-    fetch("http://localhost:3000/api/v1/testemonials")
-      .then((response) => response.json())
-      .then((result) => {
-        dispatch(testimonialImportAction(result));
-      })
-      .catch(() => null);
-  }, []);
+		fetch("http://localhost:3000/api/v1/testemonials")
+			.then((response) => response.json())
+			.then((result) => {
+				dispatch(testimonialImportAction(result));
+			})
+			.catch(() => null);
+	}, []);
 
-  return (
-    <div className="testimonial-section">
-      <div className="teastimonial-text">
-        <div>
-          <Swiper
-            id="main"
-            tag="ul"
-            pagination
-            pagination={{ clickable: true }}
-            slidesPerView={1}
-          >
-            {testimonials.data.map((testimonial, index) => {
-              return (
-                <SwiperSlide tag="li" key={index}>
-                  <div>
-                    <img
-                      className="testimonial-img"
-                      src={
-                        "./images/" +
-                        testimonial.postedBy.split(" ")[0] +
-                        ".jpg"
-                      }
-                    />
-                  </div>
-                  <h2 className="char">“</h2>
-                  <p className="testimonial-description">
-                    {testimonial.description}
-                  </p>
-                  <p className="testimonial-by">{testimonial.postedBy}</p>
-                  <p className="testimonial-date">
-                    {formatDate(testimonial.postedOn)}
-                  </p>
-                </SwiperSlide>
-              );
-            })}
-          </Swiper>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="testimonial-section">
+			<div className="teastimonial-slider">
+
+				<Swiper
+					id="main"
+					tag="ul"
+					pagination
+					pagination={{ clickable: true }}
+					slidesPerView={1}
+				>
+					{testimonials.data.map((testimonial, index) => {
+						return (
+							<SwiperSlide tag="li" key={index}>
+								<div className="testimonial-slide">
+									<img
+										className="testimonial-img"
+										src={
+											"./images/" +
+											testimonial.postedBy.split(" ")[0] +
+											".jpg"
+										}
+									/>
+									<div className="testimonial-block">
+										<h2 className="char">“</h2>
+										<div className="testimonial-info">
+											<p className="testimonial-description">
+												{testimonial.description}
+											</p>
+											<p className="testimonial-by">{testimonial.postedBy}</p>
+											<p className="testimonial-date">
+												{formatDate(testimonial.postedOn)}
+											</p>
+										</div>
+									</div>
+								</div>
+							</SwiperSlide>
+						);
+					})}
+				</Swiper>
+			</div>
+		</div>
+	);
 };
 
 export default Testimonial;
