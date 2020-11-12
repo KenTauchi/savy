84ecbfb6f86feada45cdf18278f32edaa5d63bf8
Ken Tauchi <kentauchica@gmail.com>
2020-11-12 12:19:11 -0800

    pie chart responsivenss on realtime change in innerWidth

--- front-end/src/reducks/mapChartData/operations.js
+++ front-end/src/reducks/mapChartData/operations.js
@@ -1,33 +1,33 @@
-// import { dataImportAction } from "./action";
-import { API_URL } from '../../components/global_variables';
+// // import { dataImportAction } from "./action";
+// import { API_URL } from '../../components/global_variables';
 
-export const dataFetch = () => {
-  // return (dispatch, getState) => {
-    const provs = fetch(
-      // "https://cors-anywhere.herokuapp.com/" +
-      //   "https://api.covid19tracker.ca/provinces"
-      `${API_URL}/provinces`
-    )
-      .then((response) => response.json())
-      .catch(() => null);
+// export const dataFetch = () => {
+//   // return (dispatch, getState) => {
+//     const provs = fetch(
+//       // "https://cors-anywhere.herokuapp.com/" +
+//       //   "https://api.covid19tracker.ca/provinces"
+//       `${API_URL}/provinces`
+//     )
+//       .then((response) => response.json())
+//       .catch(() => null);
 
-    let provDataArr = [];
+//     let provDataArr = [];
 
-    provs.forEach((prov) => {
-      let provId = "CA-" + prov.code;
-      let provVal = prov.population;
-      let provObj = {
-        id: provId,
-        value: provVal,
-      };
+//     provs.forEach((prov) => {
+//       let provId = "CA-" + prov.code;
+//       let provVal = prov.population;
+//       let provObj = {
+//         id: provId,
+//         value: provVal,
+//       };
 
-      provDataArr.push(provObj);
-    });
-    console.log(provDataArr)
+//       provDataArr.push(provObj);
+//     });
+//     console.log(provDataArr)
 
-    return provDataArr
-    // dispatch(
-    //   dataImportAction(provDataArr)
-    // );
-  // };
-};
+//     return provDataArr
+//     // dispatch(
+//     //   dataImportAction(provDataArr)
+//     // );
+//   // };
+// };
