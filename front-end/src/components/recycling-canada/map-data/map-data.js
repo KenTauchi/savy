e2a876c57a1a3bb4fcb8de07335437661a121f2b
Ken Tauchi <kentauchica@gmail.com>
2020-11-14 18:43:49 -0800

    add default data to mapdatatable

--- front-end/src/components/recycling-canada/map-data/map-data.js
+++ front-end/src/components/recycling-canada/map-data/map-data.js
@@ -1,4 +1,5 @@
-import React from "react";
+import { repeat } from "@amcharts/amcharts4/.internal/core/utils/String";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { getDataSet } from "../../../reducks/mapChartData/selectors";
 
@@ -8,10 +9,42 @@ const MapData = () => {
   const table = mapTableData.mapDataTable;
   //   console.log("province", table);
 
+  const minMinStyle = {
+    gridTemplateColumns: "repeat(2, 1fr)",
+  };
+  const minMidStyle = {
+    gridTemplateColumns: "repeat(3, 1fr)",
+  };
+  const minMidAltStyle = {
+    gridTemplateColumns: "repeat(4, 1fr)",
+  };
+  const minMaxStyle = {
+    gridTemplateColumns: "repeat(6, 1fr)",
+  };
+
+  const [width, setWidth] = useState(0);
+  const updateSize = () => setWidth(window.innerWidth);
+
+  const ulStyle = () =>
+    width < 700
+      ? minMinStyle
+      : width > 700 && width < 1024 && table.prov_Rank <= 0
+      ? minMidAltStyle
+      : width > 700 && width < 1024 && table.prov_Rank > 0
+      ? minMidStyle
+      : width > 1024 && table.prov_Rank <= 0
+      ? minMidAltStyle
+      : minMaxStyle;
+
+  useEffect(() => {
+    window.addEventListener("resize", updateSize);
+    updateSize();
+  }, [width]);
+
   return (
     <div className="map-data-section">
       <h2>{table.provinceName}</h2>
-      <ul>
+      <ul style={ulStyle()}>
         {/* Waste Recycled */}
         <li className="table-data">
           <span>
@@ -47,34 +80,38 @@ const MapData = () => {
         </li>
 
         {/* Province Rank */}
-        <li className="table-data">
-          <span>
-            <img
-              className="map-data-img"
-              src="./images/icons/province-rank.svg"
-              alt=""
-            />
-          </span>
-          <span className="map-data-value">
-            {table.prov_Rank ? table.prov_Rank : "0"}
-          </span>
-          <span className="data-table-title">Province Rank</span>
-        </li>
+
+        {table.prov_Rank <= 0 ? null : (
+          <li className="table-data">
+            <span>
+              <img
+                className="map-data-img"
+                src="./images/icons/province-rank.svg"
+                alt=""
+              />
+            </span>
+            <span className="map-data-value">{table.prov_Rank}</span>
+            <span className="data-table-title">Province Rank</span>
+          </li>
+        )}
 
         {/* Population Contirbution Rank */}
-        <li className="table-data">
-          <span>
-            <img
-              className="map-data-img"
-              src="./images/icons/population-contribution-rank.svg"
-              alt=""
-            />
-          </span>
-          <span className="map-data-value">
-            {table.prov_Population_Rank ? table.prov_Population_Rank : "0"}
-          </span>
-          <span className="data-table-title">Population Contirbution Rank</span>
-        </li>
+
+        {table.prov_Population_Rank <= 0 ? null : (
+          <li className="table-data">
+            <span>
+              <img
+                className="map-data-img"
+                src="./images/icons/population-contribution-rank.svg"
+                alt=""
+              />
+            </span>
+            <span className="map-data-value">{table.prov_Population_Rank}</span>
+            <span className="data-table-title">
+              Population Contirbution Rank
+            </span>
+          </li>
+        )}
 
         {/* Industries */}
         <li className="table-data">
