1f89309b547c1a65e5f2650a5cee1a3f94be67e8
H J Vasoya <65635239+hjvasoya009@users.noreply.github.com>
2020-11-26 12:05:35 -0800

    Team Slider update

--- front-end/src/components/recycling-canada/team/team-member.js
+++ front-end/src/components/recycling-canada/team/team-member.js
@@ -2,16 +2,40 @@ import React from 'react';
 import * as FAIcons from "react-icons/fa";
 
 const TeamMember = (props) => {
+    let borderColor = "";
+    switch (props.memberName) {
+
+        case 'Vinicius Meyer Lana':
+        case 'Roujyar Darvish':
+        case 'Navneet Kaur':
+        case 'Hardikkumar Vasoya':
+            borderColor = "#f2a9a3";
+            break;
+
+        case 'Hiteshri Nanda':
+        case 'Shentario Nikogiani':
+        case 'Bhumili Kalra':
+            borderColor = "#f8edc0";
+            break;
+
+        case 'Ken Tauchi':
+        case 'Tomohiro Yoshida':
+        case 'Jaqueline Santos':
+        case 'Gurvinder Singh':
+            borderColor = '#87d3c6';
+            break;
+    }
+
     return (
         <div className="team-member-info">
             <img src={"./images/" + props.imageSRC} />
             <div className="member-info">
-                <p>{props.memberName}</p>
-                <p>{props.memberRole}</p>
+                <p className="member-name">{props.memberName}</p>
+                <p className="member-role" style={{ borderColor: borderColor }}>{props.memberRole}</p>
                 <p className="member-profile">
-                    {props.linkedIn != "" ? <a href={props.linkedIn} style={{color:"#0E76A8"}} target="_blank"><FAIcons.FaLinkedin /></a> : ""}
-                    {props.gitHub != "" ? <a href={props.gitHub} style={{color:"#7DBBE6"}} target="_blank"><FAIcons.FaGithubSquare /></a> : ""}
-                    {props.behance != "" ? <a href={props.behance} style={{color:"#053EFF"}} target="_blank"><FAIcons.FaBehanceSquare /></a> : ""}
+                    {props.linkedIn != "" ? <a href={props.linkedIn} style={{ color: "#0E76A8" }} target="_blank"><FAIcons.FaLinkedin /></a> : ""}
+                    {props.gitHub != "" ? <a href={props.gitHub} style={{ color: "#7DBBE6" }} target="_blank"><FAIcons.FaGithubSquare /></a> : ""}
+                    {props.behance != "" ? <a href={props.behance} style={{ color: "#053EFF" }} target="_blank"><FAIcons.FaBehanceSquare /></a> : ""}
                 </p>
             </div>
         </div>
