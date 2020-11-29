65300a02e054a78bbe5fa145e79e99e0d9740271
TOMO-YOSHI <lucky.paddy10@gmail.com>
2020-11-28 23:38:44 -0800

    modify search redux

--- front-end/src/components/waste-management/google-map/GoogleMap.component.js
+++ front-end/src/components/waste-management/google-map/GoogleMap.component.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import GoogleMapReact from "google-map-react";
 
 import Marker from "../marker/Marker.component";
